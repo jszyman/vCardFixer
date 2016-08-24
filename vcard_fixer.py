f18fa0daec1179eafe1a3e42033c41c83c42ee1f
jszyman <jszyman@users.noreply.github.com>
2016-08-24 21:51:59 +0200

    file operations are done on files coming from IN and results are stored in OUT

--- vcard_fixer.py
+++ vcard_fixer.py
@@ -8,23 +8,32 @@ def open_file(self, file_name, path = './', mode = 'r'):
     except IOError:
         return None
 
-vcard_file = open("Name Lastname.vcf")
+vcard_file = open(".\in\Jon Doe.vcf")
 vcard_text = vcard_file.read(-1)
 vcard_file.close()
 
 j = vobject.readOne(vcard_text)
-j.add('fn')
 j.prettyPrint()
+
 print
-print j.n.value.family
-print j.n.value.given
+print "Family name: " + j.n.value.family
+print "Given name: " + j.n.value.given
 
 #exchange given and family names for fixing sake
 j.n.value.family, j.n.value.given = j.n.value.given, j.n.value.family
 
 j.prettyPrint()
 
-print j.serialize()
+#serialize() requires at least 1 FN field 
+j.add('fn')
+#print j.serialize()
+
+#store reworked file
+file_out =  open(".\out\Jon Doe.vcf", "w")
+file_out.write(j.serialize())
+file_out.close()
 
 if __name__ == '__main__':
     pass
+
+#TODO: remove empty lines from out-files
